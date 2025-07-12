42b13072cb6f45ed11f8ad64e72a03bdb8660498
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-07-12 14:17:02 +0300

    Q4 solve

--- Assignment 2 OOP/Program.cs
+++ Assignment 2 OOP/Program.cs
@@ -1,6 +1,8 @@
 ﻿using DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml.Drawing.Charts;
 using DocumentFormat.OpenXml.Presentation;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Assignment_2_OOP
@@ -30,15 +32,63 @@ namespace Assignment_2_OOP
 
 
     //Q3
-    public struct Person
+    //public struct Person
+    //{
+    //    public string Name { get; set; }
+    //    public int Age { get; set; }
+
+
+    //    public override string ToString()
+    //    {
+    //        return $"Name: {Name}, Age: {Age}";
+    //    }
+    //}
+
+
+
+    public struct Rectangle
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
+        private double width;
+        private double height;
 
-
-        public override string ToString()
+        public double Width
         {
-            return $"Name: {Name}, Age: {Age}";
+            get => width;
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Width cannot be negative.");
+                }
+                else
+                {
+                    width = value;
+                }
+            }
+        }
+        public double Hight
+        {
+            get => Hight;
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Width cannot be negative.");
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+
+        public double Area => width * height;
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Rectangle Dimensions: Width = {width}, Height = {height}, Area = {Area}");
         }
     }
     internal class Program
@@ -81,25 +131,37 @@ namespace Assignment_2_OOP
 
 
             #region 3-Create a struct called "Person" with properties "Name" and "Age". Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.   
-            Person[] persons = new Person[3];
-            int mxAge = 0, Pointer = 0;
-            for (int i = 0; i < persons.Length; i++)
-            {
-                Console.WriteLine($"Enter details for person {i + 1}:");
-                Console.Write("Name: ");
-                persons[i].Name = Console.ReadLine();
-                Console.Write("Age: ");
-                persons[i].Age = int.Parse(Console.ReadLine());
-                if (persons[i].Age > mxAge)
-                {
-                    mxAge = persons[i].Age;
-                    Pointer = i;
-                }
-            }
-            Console.WriteLine($"The oldest person is: {persons[Pointer].Name} with age {persons[Pointer].Age}");
+            //Person[] persons = new Person[3];
+            //int mxAge = 0, Pointer = 0;
+            //for (int i = 0; i < persons.Length; i++)
+            //{
+            //    Console.WriteLine($"Enter details for person {i + 1}:");
+            //    Console.Write("Name: ");
+            //    persons[i].Name = Console.ReadLine();
+            //    Console.Write("Age: ");
+            //    persons[i].Age = int.Parse(Console.ReadLine());
+            //    if (persons[i].Age > mxAge)
+            //    {
+            //        mxAge = persons[i].Age;
+            //        Pointer = i;
+            //    }
+            //}
+            //Console.WriteLine($"The oldest person is: {persons[Pointer].Name} with age {persons[Pointer].Age}");
             #endregion
 
+
+            #region 4.Create a struct named Rectangle that represents a rectangle with the following fields:  width(type: double)height(type: double)Implement encapsulation by making the fields private and provide public properties access and modify these values.Ensure the following conditions are met:The width and height should not be negative. If a negative value is provided, the setter should not update the field and should instead print an error message.Implement a public read-only property Area that calculates and returns the area of the rectangle(Area = width * height).Implement a method DisplayInfo that prints the rectangle's dimensions and area.Write a program to demonstrate the usage of this struct by creating an instance, setting values via properties, and displaying the area.
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine("Enter the width of the rectangle:");
+            rectangle.Width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height of the rectangle:");
+            rectangle.Hight = double.Parse(Console.ReadLine());
+
+            rectangle.DisplayInfo();
+
+            #endregion
         }
     }
+
 
 }
